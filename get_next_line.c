9db91765bef8215da44e21a503b369a4dff10a91
Pablo Fajardo Mayor <pabfajar@c3r5s4.42malaga.com>
2026-05-19 19:34:46 +0200

    get_next_line v3

--- get_next_line.c
+++ get_next_line.c
@@ -6,36 +6,51 @@
 /*   By: pabfajar <pabfajar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 18:06:57 by pabfajar          #+#    #+#             */
-/*   Updated: 2026/05/19 12:53:26 by pabfajar         ###   ########.fr       */
+/*   Updated: 2026/05/19 19:32:33 by pabfajar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+int	ft_read_line(char *buffer, int fd)
 {
-	static char	*store;
-	char		*buffer;
-	int			bytes;
-	char		*line;
+	int	bytes;
 
-	if (BUFFER_SIZE <= 0 || fd < 0)
-	{
-		free(store);
-		return (NULL);
-	}
-	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!buffer)
-	{
-		free(buffer);
-		return (NULL);
-	}
 	bytes = read(fd, buffer, BUFFER_SIZE);
 	if (bytes <= 0)
 		return (NULL);
 	buffer[bytes] = '\0';
-	store = ft_strjoin(store, buffer);
-	if (ft_strchr(store, '\n'))
-		line = ft_extract_line(store);
+	return (bytes);
+}
+
+char	*ft_get_excess(char *store, char *str)
+{
+	int		pos;
+	char	*excess;
+	
+	pos = 0;
+	while (str[pos + 1])
+		excess[pos] = str[pos + 1];
+	return (excess);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*store;
+	char		*buffer;
+	char		*line;
+	char		*excess;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	while (!ft_strchr(store, '\n') && ft_read_line(buffer, fd))
+	{
+		store = ft_strjoin(store, buffer);
+		free (buffer);
+	}
+	store = ft_get_excess(store, ft_strchr(store, '\n'));
 	return (line);
 }
